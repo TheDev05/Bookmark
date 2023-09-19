7943a24afc38e8540c30adfeb8bed9147eabc26e
TheDev05 <a.singh725098@gmail.com>
2023-09-19 14:17:44 +0530

    UPD

--- project/Backend/index.js
+++ project/Backend/index.js
@@ -4,15 +4,17 @@ const port = 5000;
 
 const mongoDB = require("./db");
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+const cors = require("cors");
+app.use(cors());
 
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 
 app.use(express.json());
 app.use("/api", require("./Routes/CreateUser"));
@@ -21,7 +23,7 @@ app.use("/api", require("./Routes/OrderData"));
 app.use("/api", require("./Routes/Profile"));
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.json("Hello");
 });
 
 app.listen(port, () => {
