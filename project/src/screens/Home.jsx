2d01755a7697bbe7d5e031c84598cd58f8978320
TheDev05 <a.singh725098@gmail.com>
2023-09-19 14:28:22 +0530

    UPD

--- project/src/screens/Home.jsx
+++ project/src/screens/Home.jsx
@@ -5,6 +5,8 @@ import Footer from "../components/Footer";
 import style from "./Home.module.css";
 import { ImBooks } from "react-icons/im";
 
+import img1 from "../assests/title_img.jpg"
+
 export default function () {
   const [foodCat, setFoodCat] = useState([]);
   const [foodItem, setFoodItem] = useState([]);
@@ -50,7 +52,7 @@ export default function () {
         </div>
         <div className={style.right}>
           <img
-            src="https://private-user-images.githubusercontent.com/73834703/268693071-d0ccdb43-e9c0-4ceb-8fe4-08c2b72772c9.jpg?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTEiLCJleHAiOjE2OTUwNTA5MDcsIm5iZiI6MTY5NTA1MDYwNywicGF0aCI6Ii83MzgzNDcwMy8yNjg2OTMwNzEtZDBjY2RiNDMtZTljMC00Y2ViLThmZTQtMDhjMmI3Mjc3MmM5LmpwZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFJV05KWUFYNENTVkVINTNBJTJGMjAyMzA5MTglMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjMwOTE4VDE1MjMyN1omWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWQ1MjM1MGE2ODA2YTVlYzEwM2IyNzI4MDZhYjZiN2ExZWM5ODZkYTZmZjk1NjJhYzM3NDcyYTBmMTM0NDgzNWYmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.4iLZOwUXxZiFL6Kf_8Dw_1Qnk_wa8V9Zk_xavogvEvU"
+            src={img1}
             alt=""
           />
         </div>
