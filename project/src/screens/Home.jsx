dee47fa9416a1fc5dbad7a9cef39c8e9050d9419
Ankit D <a.singh725098@gmail.com>
2024-02-10 23:07:24 +0530

    Update Home.jsx

--- project/src/screens/Home.jsx
+++ project/src/screens/Home.jsx
@@ -51,12 +51,12 @@ export default function () {
     loadData();
   }, []);
 
-  if (foodItem.length == 0)
-    return (
-      <div className={style.loader}>
-        <span>Stacking Books, Hold Tight!</span>
-      </div>
-    );
+  // if (foodItem.length == 0)
+  //   return (
+  //     <div className={style.loader}>
+  //       <span>Stacking Books, Hold Tight!</span>
+  //     </div>
+  //   );
 
   let dispatch = useDispatchCart();
   let data = useCart();
@@ -88,141 +88,148 @@ export default function () {
     // console.log("props",  props);
   };
 
-  return (
-    <div className={style.body}>
-      <div className={style.featured}>
-        <div className={style.left}>
-          <div className={style.para}>
-            <h1>
-              Life of The <br></br>Wild
-            </h1>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque
-              eos numquam unde necessitatibus, ipsa eum, accusantium doloremque
-              nisi natus soluta maiores ?
-            </p>
-            <button onClick={addtocart}>
-              <span>Add to Cart</span>
-            </button>
+return (
+    <>
+      {foodItem.length > 0 ? (
+        <div className={style.body}>
+          <div className={style.featured}>
+            <div className={style.left}>
+              <div className={style.para}>
+                <h1>
+                  Life of The <br></br>Wild
+                </h1>
+                <p>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  Cumque eos numquam unde necessitatibus, ipsa eum, accusantium
+                  doloremque nisi natus soluta maiores ?
+                </p>
+                <button onClick={addtocart}>
+                  <span>Add to Cart</span>
+                </button>
+              </div>
+            </div>
+            <div className={style.right}>
+              <img src={img1} alt="" />
+            </div>
           </div>
-        </div>
-        <div className={style.right}>
-          <img src={img1} alt="" />
-        </div>
-      </div>
 
-      <div className={style.partion}>
-        <div className={style.holder}>
-          <div className={style.line1}></div>
-          {/* <ImBooks className={style.icons} />{" "}
-          <div className={style.line1}></div> */}
-        </div>
-      </div>
+          <div className={style.partion}>
+            <div className={style.holder}>
+              <div className={style.line1}></div>
+              {/* <ImBooks className={style.icons} />{" "}
+        <div className={style.line1}></div> */}
+            </div>
+          </div>
 
-      <div className={style.book_section}>
-        <div className={style.title}>
-          <h2>Popular Books</h2>
-        </div>
-        <div className={style.genre}>
-          <button
-            onClick={() => {
-              setGenre("All Genres");
-            }}
-          >
-            All Genres
-          </button>
-          <button
-            onClick={() => {
-              setGenre("Business");
-            }}
-          >
-            Business
-          </button>
-          <button
-            onClick={() => {
-              setGenre("Technology");
-            }}
-          >
-            Technology
-          </button>
-          <button
-            onClick={() => {
-              setGenre("Romantic");
-            }}
-          >
-            Romantic
-          </button>
-          <button
-            onClick={() => {
-              setGenre("Adventure");
-            }}
-          >
-            Adventure
-          </button>
-          <button
-            onClick={() => {
-              setGenre("Fictinol");
-            }}
-          >
-            Fictional
-          </button>
-        </div>
+          <div className={style.book_section}>
+            <div className={style.title}>
+              <h2>Popular Books</h2>
+            </div>
+            <div className={style.genre}>
+              <button
+                onClick={() => {
+                  setGenre("All Genres");
+                }}
+              >
+                All Genres
+              </button>
+              <button
+                onClick={() => {
+                  setGenre("Business");
+                }}
+              >
+                Business
+              </button>
+              <button
+                onClick={() => {
+                  setGenre("Technology");
+                }}
+              >
+                Technology
+              </button>
+              <button
+                onClick={() => {
+                  setGenre("Romantic");
+                }}
+              >
+                Romantic
+              </button>
+              <button
+                onClick={() => {
+                  setGenre("Adventure");
+                }}
+              >
+                Adventure
+              </button>
+              <button
+                onClick={() => {
+                  setGenre("Fictinol");
+                }}
+              >
+                Fictional
+              </button>
+            </div>
 
-        <div className={style.books}>
-          {foodItem.length !== 0 ? (
-            foodItem.map((e) => {
-              return e.CategoryName === genre || genre == "All Genres" ? (
-                <div key={e._id}>
-                  <Card foodName={e} />
-                </div>
+            <div className={style.books}>
+              {foodItem.length !== 0 ? (
+                foodItem.map((e) => {
+                  return e.CategoryName === genre || genre == "All Genres" ? (
+                    <div key={e._id}>
+                      <Card foodName={e} />
+                    </div>
+                  ) : (
+                    <></>
+                  );
+                })
               ) : (
-                <></>
-              );
-            })
-          ) : (
-            <div>No data is found</div>
-          )}
+                <div>No data is found</div>
+              )}
+            </div>
+          </div>
+          <Footer />
         </div>
-      </div>
-      <Footer />
-    </div>
+      ) : (
+        // <div>
+        //   <div>
+        //     <input
+        //       type="search"
+        //       name="search"
+        //       placeholder="search here"
+        //       value={search}
+        //       onChange={(e) => {
+        //         setSearch(e.target.value);
+        //       }}
+        //     />
+        //   </div>
 
-    // <div>
-    //   <div>
-    //     <input
-    //       type="search"
-    //       name="search"
-    //       placeholder="search here"
-    //       value={search}
-    //       onChange={(e) => {
-    //         setSearch(e.target.value);
-    //       }}
-    //     />
-    //   </div>
+        //   <div className="container">
+        //     {foodItem.length !== 0 ? (
+        //       foodItem
+        //         .filter(
+        //           (item) =>
+        //             item.CategoryName === "Starter" &&
+        //             item.name.toLowerCase().includes(search.toLowerCase())
+        //         )
+        //         .map((filterItems) => {
+        //           return (
+        //             <div key={filterItems._id} className="col-12 col-md">
+        //               <Card foodName={filterItems} />
+        //             </div>
+        //           );
+        //         })
+        //     ) : (
+        //       <div>No data is found</div>
+        //     )}
+        //   </div>
 
-    //   <div className="container">
-    //     {foodItem.length !== 0 ? (
-    //       foodItem
-    //         .filter(
-    //           (item) =>
-    //             item.CategoryName === "Starter" &&
-    //             item.name.toLowerCase().includes(search.toLowerCase())
-    //         )
-    //         .map((filterItems) => {
-    //           return (
-    //             <div key={filterItems._id} className="col-12 col-md">
-    //               <Card foodName={filterItems} />
-    //             </div>
-    //           );
-    //         })
-    //     ) : (
-    //       <div>No data is found</div>
-    //     )}
-    //   </div>
-
-    //   <div>
-    // <Footer />
-    //   </div>
-    // </div>
+        //   <div>
+        // <Footer />
+        //   </div>
+        // </div>
+        <div className={style.loader}>
+          <span>Stacking Books, Hold Tight!</span>
+        </div>
+      )}
+    </>
   );
 }
