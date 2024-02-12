cd8096822e32b876a7c5db5d340aecbfbbd2792c
Ankit D <a.singh725098@gmail.com>
2024-02-12 22:33:54 +0530

    Update Home.jsx

--- project/src/screens/Home.jsx
+++ project/src/screens/Home.jsx
@@ -1,25 +1,14 @@
 import React, { useState, useEffect } from "react";
-import Card from "../components/Card";
-import Footer from "../components/Footer";
+import Card from "../components/Card.jsx";
+import Footer from "../components/Footer.jsx";
 import style from "./Home.module.css";
 import img1 from "../assests/title_img.jpg";
-import { styled } from "@mui/material/styles";
-import Paper from "@mui/material/Paper";
 import { useCart, useDispatchCart } from "../components/ContextReducer.js";
 import toast, { Toaster } from "react-hot-toast";
-
-// const Item = styled(Paper)(({ theme }) => ({
-//   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-//   ...theme.typography.body2,
-//   padding: theme.spacing(1),
-//   textAlign: "center",
-//   color: theme.palette.text.secondary,
-// }));
 
 export default function () {
   const [foodCat, setFoodCat] = useState([]);
   const [foodItem, setFoodItem] = useState([]);
-  // const [search, setSearch] = useState("");
 
   const [genre, setGenre] = useState("All Genres");
   const [isLoading, setIsLoading] = useState(true);
@@ -35,16 +24,16 @@ export default function () {
     response = await response.json();
 
     setFoodCat(response[1]);
-    console.log(response[0]);
+    setFoodItem(response[0]);
 
-    if (response[0] != null) setFoodItem(response[0]);
-    setIsLoading(false);
+    // console.log(response[0]);
+    if (response[0] != null) setIsLoading(false);
   };
 
   useEffect(() => {
     setIsLoading(true);
     loadData();
-  }, []);
+  }, [isLoading]);
 
   let dispatch = useDispatchCart();
   let data = useCart();
@@ -78,146 +67,101 @@ export default function () {
 
   return (
     <>
-      {isLoading === false ? (
-        <div className={style.body}>
-          <div className={style.featured}>
-            <div className={style.left}>
-              <div className={style.para}>
-                <h1>
-                  Life of The <br></br>Wild
-                </h1>
-                <p>
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                  Cumque eos numquam unde necessitatibus, ipsa eum, accusantium
-                  doloremque nisi natus soluta maiores ?
-                </p>
-                <button onClick={addtocart}>
-                  <span>Add to Cart</span>
-                </button>
-              </div>
-            </div>
-            <div className={style.right}>
-              <img src={img1} alt="" />
+      <div className={style.body}>
+        <div className={style.featured}>
+          <div className={style.left}>
+            <div className={style.para}>
+              <h1>
+                Life of The <br></br>Wild
+              </h1>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque
+                eos numquam unde necessitatibus, ipsa eum, accusantium
+                doloremque nisi natus soluta maiores ?
+              </p>
+              <button onClick={addtocart}>
+                <span>Add to Cart</span>
+              </button>
             </div>
           </div>
-
-          <div className={style.partion}>
-            <div className={style.holder}>
-              <div className={style.line1}></div>
-              {/* <ImBooks className={style.icons} />{" "}
-        <div className={style.line1}></div> */}
-            </div>
+          <div className={style.right}>
+            <img src={img1} alt="" />
           </div>
-
-          <div className={style.book_section}>
-            <div className={style.title}>
-              <h2>Popular Books</h2>
-            </div>
-            <div className={style.genre}>
-              <button
-                onClick={() => {
-                  setGenre("All Genres");
-                }}
-              >
-                All Genres
-              </button>
-              <button
-                onClick={() => {
-                  setGenre("Business");
-                }}
-              >
-                Business
-              </button>
-              <button
-                onClick={() => {
-                  setGenre("Technology");
-                }}
-              >
-                Technology
-              </button>
-              <button
-                onClick={() => {
-                  setGenre("Romantic");
-                }}
-              >
-                Romantic
-              </button>
-              <button
-                onClick={() => {
-                  setGenre("Adventure");
-                }}
-              >
-                Adventure
-              </button>
-              <button
-                onClick={() => {
-                  setGenre("Fictinol");
-                }}
-              >
-                Fictional
-              </button>
-            </div>
-
-            <div className={style.books}>
-              {foodItem.length !== 0 ? (
-                foodItem.map((e) => {
-                  return e.CategoryName === genre || genre == "All Genres" ? (
-                    <div key={e._id}>
-                      <Card foodName={e} />
-                    </div>
-                  ) : (
-                    <></>
-                  );
-                })
-              ) : (
-                <div>No data is found</div>
-              )}
-            </div>
-          </div>
-          <Footer />
         </div>
-      ) : (
-        // <div>
-        //   <div>
-        //     <input
-        //       type="search"
-        //       name="search"
-        //       placeholder="search here"
-        //       value={search}
-        //       onChange={(e) => {
-        //         setSearch(e.target.value);
-        //       }}
-        //     />
-        //   </div>
 
-        //   <div className="container">
-        //     {foodItem.length !== 0 ? (
-        //       foodItem
-        //         .filter(
-        //           (item) =>
-        //             item.CategoryName === "Starter" &&
-        //             item.name.toLowerCase().includes(search.toLowerCase())
-        //         )
-        //         .map((filterItems) => {
-        //           return (
-        //             <div key={filterItems._id} className="col-12 col-md">
-        //               <Card foodName={filterItems} />
-        //             </div>
-        //           );
-        //         })
-        //     ) : (
-        //       <div>No data is found</div>
-        //     )}
-        //   </div>
-
-        //   <div>
-        // <Footer />
-        //   </div>
-        // </div>
-        <div className={style.loader}>
-          <span>Stacking Books, Hold Tight!</span>
+        <div className={style.partion}>
+          <div className={style.holder}>
+            <div className={style.line1}></div>
+          </div>
         </div>
-      )}
+
+        <div className={style.book_section}>
+          <div className={style.title}>
+            <h2>Popular Books</h2>
+          </div>
+          <div className={style.genre}>
+            <button
+              onClick={() => {
+                setGenre("All Genres");
+              }}
+            >
+              All Genres
+            </button>
+            <button
+              onClick={() => {
+                setGenre("Business");
+              }}
+            >
+              Business
+            </button>
+            <button
+              onClick={() => {
+                setGenre("Technology");
+              }}
+            >
+              Technology
+            </button>
+            <button
+              onClick={() => {
+                setGenre("Romantic");
+              }}
+            >
+              Romantic
+            </button>
+            <button
+              onClick={() => {
+                setGenre("Adventure");
+              }}
+            >
+              Adventure
+            </button>
+            <button
+              onClick={() => {
+                setGenre("Fictinol");
+              }}
+            >
+              Fictional
+            </button>
+          </div>
+
+          <div className={style.books}>
+            {foodItem != null && foodItem.length != 0 ? (
+              foodItem.map((e) => {
+                return e.CategoryName === genre || genre == "All Genres" ? (
+                  <div key={e._id}>
+                    <Card foodName={e} />
+                  </div>
+                ) : (
+                  <></>
+                );
+              })
+            ) : (
+              <div>No data is found</div>
+            )}
+          </div>
+        </div>
+        <Footer />
+      </div>
     </>
   );
 }
